b96371c55ac740d1df062ac00ef6b43dcebb2b74
vipransh <vipranshverma385@gmail.com>
2022-12-08 18:28:46 +0530

    crypto tracker

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import '../App.css'
 import homeStore from "../store/homeStore";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+
 
 
 function Home(){
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../App.css'
 import showStore from "../store/showStore";
 import { useParams } from "react-router-dom";
 import {
